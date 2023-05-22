7de5df521c238168b1150e5131bb17a436ae7eca
TheYuhan-Lu <theyuhanlu@gmail.com>
2023-05-22 20:44:16 +0800

    except url address2

--- cutiesound/__init__.py
+++ cutiesound/__init__.py
@@ -48,56 +48,30 @@ def result():
     }
     selected_labels = [options_labels[option] for option in selected_options]
     
-    generation_status = "Generating"
     
-    generation_thread = threading.Thread(target=generate_data, args=(a, selected_options, generation_status))
-    generation_thread.start()
-    result_data = generate_data(a, selected_options, generation_status)
     
-    return render_template('result.html', a=a, selected_labels=selected_labels,generation_status=generation_status,result_data=result_data)
+    return render_template('result.html', a=a, selected_labels=selected_labels)
 
-def generate_data(a, selected_options, generation_status):
-    # 在生成函数执行期间，将生成状态参数设置为"Generating"
-    # 在生成完成后，将生成状态参数设置为"Result"
-    try:
-        # 调用生成函数
-        result_data = get_random_url(a)
-    
-        # 生成完成后，将生成状态参数设置为"Result"
-        generation_status = "Result"
-
-        # 在这里进行其他后续操作，如保存文件等
-        return result_data
-    
-    except Exception as e:
-        # 处理生成函数的异常情况
-        generation_status = "Error"
-        return None
-
-def get_random_url(a):
-  
-    if a == 64:
-        url_midi = get_random_midi_url('out/64',64)
-    elif a == 128:
-        url_midi = get_random_midi_url('out/128',128)
-    else:
-        url_midi = None  # 处理其他情况
+def get_midi_url(length):
     
     time.sleep(5)  # 添加延时，单位为秒
-    
-    return f'{url_midi}'
-
-def get_random_midi_url(directory,a):
-    # 假设在指定目录中有多个MIDI文件
-    # 这里使用随机数选择一个文件
-    
-    if a == 64:
+    if length == 64:
+        folder = '64'
         midi_files = ['64-1.mid', '64-2.mid', '64-3.mid']
-    elif a == 128:
+    elif length == 128:
+        folder = '128'
         midi_files = ['128-1.mid', '128-2.mid', '128-3.mid','128-4.mid', '128-5.mid', '128-6.mid']
-    
+    else:
+        raise ValueError('Invalid length.')
+
+    # 在特定目录下随机选择一个.mid文件
+    # 这里使用了示例的链接，你可以根据需要修改
+    base_url = 'https://cdn.jsdelivr.net/gh/TheYuhan-Lu/CutieSound@1.0/cutiesound/out'
+     # 这里需要替换为实际的文件列表
     random_file = random.choice(midi_files)
-    return f'https://cdn.jsdelivr.net/gh/TheYuhan-Lu/CutieSound@first/cutiesound/{directory}/{random_file}'
+    midi_url = f'{base_url}/{folder}/{random_file}'
+    
+    return midi_url
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
